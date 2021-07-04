538a3deb2bc0b0d2ba2908300f69912a1a3b48da
RyoKitatani <ryo0929m@gmail.com>
2021-07-05 00:18:24 +0900

    update main.js

--- main.js
+++ main.js
@@ -3,3 +3,26 @@
 const addTaskTrigger = document.querySelector('.js-addTask-trigger');
 const addTaskTarget = document.querySelector('.js-addTask-target');
 const addTaskValue = document.querySelector('.js-addTask-value');
+
+const removeTask = removeButton => {
+  const targetTask = removeButton.closest('li');
+  addTaskTarget.removeChild(targetTask);
+}
+
+const addTask = task => {
+  const listItem = document.createElement('li');
+  const removeButton = document.createElement('button');
+
+  removeButton.innerText = '削除する';
+  removeButton.addEventListener('click', () => removeTask(removeButton));
+
+  listItem.innerText = task;
+  listItem.append(removeButton);
+  addTaskTarget.appendChild(listItem)
+};
+
+addTaskTrigger.addEventListener('click', event => {
+  const task = addTaskValue.Value;
+  addTask(task);
+  addTaskValue.value = '';
+})
