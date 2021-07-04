5f94ef0947948f3db94b2dfc1ece8a225b7c2f71
RyoKitatani <ryo0929m@gmail.com>
2021-07-05 00:08:59 +0900

    set const/main.js

--- main.js
+++ main.js
@@ -3,4 +3,3 @@
 const addTaskTrigger = document.querySelector('.js-addTask-trigger');
 const addTaskTarget = document.querySelector('.js-addTask-target');
 const addTaskValue = document.querySelector('.js-addTask-value');
-
