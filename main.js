b6694ad7e324ca85e65d147b5009abcb84e3fffe
RyoKitatani <ryo0929m@gmail.com>
2021-07-05 00:08:21 +0900

    update index.html

---
+++ main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const addTaskTrigger = document.querySelector('.js-addTask-trigger');
+const addTaskTarget = document.querySelector('.js-addTask-target');
+const addTaskValue = document.querySelector('.js-addTask-value');
+
