09538e5ea3c996787006d69c7edfbef75213af9d
RyoKitatani <ryo0929m@gmail.com>
2021-07-05 00:33:41 +0900

    add complete-aciton

--- main.js
+++ main.js
@@ -4,29 +4,35 @@ const addTaskTrigger = document.querySelector('.js-addTask-trigger');
 const addTaskTarget = document.querySelector('.js-addTask-target');
 const addTaskValue = document.querySelector('.js-addTask-value');
 
-const removeTask = removeButton => {
-  const targetTask = removeButton.closest('li');
-  addTaskTarget.removeChild(targetTask);
-}
 
 const addTask = task => {
   const listItem = document.createElement('li');
   const removeButton = document.createElement('button');
   const completeButton = document.createElement('button');
- 
+  
   removeButton.innerText = '削除する';
   removeButton.addEventListener('click', () => removeTask(removeButton));
-
+  
   completeButton.innerText = '完了した';
   completeButton.addEventListener('click', () => completeTask(completeButton));
-
-
+  
+  
   listItem.innerText = task;
   listItem.append(completeButton);
   listItem.append(removeButton);
   addTaskTarget.appendChild(listItem)
 };
 
+const removeTask = removeButton => {
+  const targetTask = removeButton.closest('li');
+  addTaskTarget.removeChild(targetTask);
+}
+
+const completeTask = completeButton => {
+  const targetTask = completeButton.closest('li');
+  targetTask.classList.add('isComplete');
+  targetTask.removeChild(completeButton);
+}
 
 addTaskTrigger.addEventListener('click', event => {
   const task = addTaskValue.Value;
